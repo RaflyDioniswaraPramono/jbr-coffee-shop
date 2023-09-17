bba803bf9960f6f4498b91202193b241ab9a8b23
Rafly Dioniswara Pramono <raflypram368@gmail.com>
2023-09-17 11:28:56 +0700

    Finished developing project

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-// // set active class for clicked navbar links
-// let linksContainer = document.getElementById("navbar");
-// let links = linksContainer.getElementsByClassName("link");
+// // // set active class for clicked navbar links
+// // let linksContainer = document.getElementById("navbar");
+// // let links = linksContainer.getElementsByClassName("link");
 
-// for (var i = 0; i < links.length; i++) {  
-//   links[i].addEventListener("click", function() {
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   });
-// }
+// // for (var i = 0; i < links.length; i++) {
+// //   links[i].addEventListener("click", function() {
+// //     var current = document.getElementsByClassName("active");
+// //     current[0].className = current[0].className.replace(" active", "");
+// //     this.className += " active";
+// //   });
+// // }
 
 // toggle hidden navbar links for tablet and phone screen
-const hiddenLinksBox = document.getElementById("hidden-navbar-links-box");  
+const hiddenLinksBox = document.getElementById("hidden-navbar-links-box");
 const toggleHiddenNavbarBox = () => {
   if (hiddenLinksBox.classList.value.includes("show")) {
     hiddenLinksBox.classList.remove("show")
   } else {
-    hiddenLinksBox.classList.add("show");    
+    hiddenLinksBox.classList.add("show");
   }
 }
 
